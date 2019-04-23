490a74e7579e27e9f83c10156c53db548fbbd898
Stalin Anudeep <stalinanudeep@gmail.com>
2019-04-23 17:38:59 +0530

    connecting database using configuration type

--- src/main/java/com/sp/resource/DataBaseConnection.java
+++ src/main/java/com/sp/resource/DataBaseConnection.java
@@ -9,7 +9,7 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
-
+import org.apache.wicket.Application;
 import com.sp.location.AddNetworkLocationDetailForm;
 
 public class DataBaseConnection {
@@ -17,6 +17,7 @@ public class DataBaseConnection {
 	private Connection con;
 	private Context envContext;
 	private static final Logger log = Logger.getLogger(DataBaseConnection.class);
+	private enum ConfigurationType {DEVELOPMENT,STAGING,DEPLOYMENT};
 	public DataBaseConnection() {
 		// TODO Auto-generated constructor stub
 	con = null;
@@ -24,10 +25,11 @@ public class DataBaseConnection {
 	}
 	public Connection getConnection()
 	{
+		ConfigurationType ctype = ConfigurationType.valueOf(Application.get().getConfigurationType().toString().toUpperCase());
 		try {
 			envContext=new InitialContext();
 			Context initContext=(Context) envContext.lookup("java:comp/env");
-			DataSource ds=(DataSource) initContext.lookup("MyDS");
+			DataSource ds=(DataSource) initContext.lookup(ctype.toString());
 			con=ds.getConnection();
 			
 		} catch (SQLException e) {
